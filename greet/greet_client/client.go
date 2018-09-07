bdbd5fc50da8eeab1d399ff70041a454be9c91ad
rene <renegmed@yahoo.com>
2018-09-07 11:48:23 -0400

    bi-directional streaming api - client implementation

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -23,11 +23,14 @@ func main() {
 	client := greetpb.NewGreetServiceClient(conn)
 	// fmt.Printf("Created client: %f\n", client)
 
-	//doUnary(client)
+	// doUnary(client)
 
 	// doServerStreaming(client)
 
-	doClientStreaming(client)
+	// doClientStreaming(client)
+
+	doBiDiStreaming(client)
+
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -122,4 +125,110 @@ func doClientStreaming(c greetpb.GreetServiceClient) {
 
 	fmt.Printf("LongGreet Response: %v\n", res)
 
+}
+func doBiDiStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a BiDi Streaming RPC....")
+
+	requests := []*greetpb.GreetEveryoneRequest{
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Stephane",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "John",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Lucy",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Mark",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Piper",
+			},
+		},
+	}
+	requests2 := []*greetpb.GreetEveryoneRequest{
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "2 Manny",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "2 Craig",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "2 Mary",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "2 Paul",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "2 Shamir",
+			},
+		},
+	}
+	// we create a stream by invoking the client
+
+	stream, err := c.GreetEveryone(context.Background())
+	if err != nil {
+		log.Fatalf("Error while creating stream: %v", err)
+	}
+
+	waitc := make(chan struct{})
+	// we send a bunch of messages to the client  (go routine)
+	go func() {
+		// function to send a bunch of messages
+		for _, req := range requests {
+			fmt.Printf("Sending message: %v\n", req)
+			stream.Send(req)
+			time.Sleep(1000 * time.Microsecond)
+		}
+		stream.CloseSend()
+	}()
+
+	// we send a bunch of messages to the client2  (go routine)
+	go func() {
+		// function to send a bunch of messages
+		for _, req := range requests2 {
+			fmt.Printf("Sending message 2: %v\n", req)
+			stream.Send(req)
+			time.Sleep(1000 * time.Microsecond)
+		}
+		stream.CloseSend()
+	}()
+	// we receive a bunch of messages from the client (go routine)
+	go func() {
+		// function to receive a bunch of messages
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error while receiving: %v", err)
+				break
+			}
+			fmt.Printf("Received: %v\n", res.GetResult())
+		}
+		close(waitc)
+	}()
+
+	// block until everything is done
+	<-waitc
 }
